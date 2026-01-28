e2b6d69b591c96be406e63c03fa83f5ab7aa2d0d
dhirenkamble <dhirenkamble06@gmail.com>
2026-01-28 13:19:28 +0530

    Add main.py for Graduate Admission Chance Prediction
    This file outlines the features used for predicting graduate admission chances.

---
+++ Graduate_Admission_Chance_Prediction/main.py
@@ -0,0 +1,17 @@
+# Graduate Admission Chance Prediction Project
+# This is a placeholder file for internship submission
+
+features = [
+    "GRE Score",
+    "TOEFL Score",
+    "CGPA",
+    "SOP",
+    "LOR",
+    "University Rating",
+    "Research Experience"
+]
+
+print("Graduate Admission Chance Prediction Project")
+print("Features used:")
+for f in features:
+    print("-", f)
